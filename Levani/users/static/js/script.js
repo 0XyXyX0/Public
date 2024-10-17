ab70d28233e1459ba4a57c4f840ace3a4ea03ce1
levani <lburdiladze7@gmail.com>
2024-10-17 20:24:41 +0400

    Add files via upload

---
+++ Levani/users/static/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('actionButton');
+    button.addEventListener('click', function() {
+        alert('Hello! You clicked the button.');
+    });
+});
